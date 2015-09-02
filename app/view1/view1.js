38e13479f89fc98b7b00dda8cecbc3446841934e
Alessandro Diaferia <alediaferia@gmail.com>
2015-09-02 16:10:57 +0100

    Setting up playground for typeahead

---
+++ app/view1/view1.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('myApp.view1', [
+    'ngRoute',
+    'ui.bootstrap'
+])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', ['$scope', function($scope) {
+    $scope.typeaheadResults = [
+        "Alessandro",
+        "Davide",
+        "Francesco"
+    ];
+}]);
